b847b7721e54f9195f468bf60876e6a266ecac5a
Jay2cof <justice101onuoha@gmail.com>
2022-11-24 16:21:00 +0100

    lambda_function_created

--- main.tf
+++ main.tf
@@ -12,18 +12,5 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 locals {
   account_id = data.aws_caller_identity.current.account_id
-  iam_role   = join(":", ["arn:iam:", local.account_id, "role/labRole"]) 
-}
-data "archive_file" "zip_the_python_code" {
-  type        =  "zip"
-  source_dir  =  "${path.module}/python/"
-  output_path =  "${path.module}/python/lambda.zip" 
-}
-resource "aws_lambda_function" "email-monitor" {
-  function_name = "email-monitor"
-  filename =  "python/lambda.zip" 
-  handler = "lambda.lambda_handler"
-  timeout = 300
-  role = local.iam_role
-  runtime = "python3.9"
+  iam_role   = join(":", ["arn:aws:iam:", local.account_id, "role/LabRole"]) 
 }
