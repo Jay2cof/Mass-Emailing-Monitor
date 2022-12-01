7ce18421e9ab1c04e96739714e8683b854a1c655
Jay2cof <justice101onuoha@gmail.com>
2022-12-01 14:19:20 +0100

    updated src file

--- monitor-server/src/main.py
+++ monitor-server/src/main.py
@@ -2,7 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-    
+def Health():
+    return {"Health": "ok"}
+
+@app.get("/job")
+def getjob():
+    return [{
+        "id": "uid1",
+        "title": "Nice Job",
+        "description": "super nice job"
+    }, {
+        "id": "uid2",
+        "title": "Nice Job2",
+        "description": "super nice job2"
+    }]
+
