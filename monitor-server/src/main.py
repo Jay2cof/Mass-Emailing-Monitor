3a53c571f9707113317b8fa449b4c1f1903ae79b
Jay2cof <justice101onuoha@gmail.com>
2022-12-15 11:05:16 +0100

    fixed the issues on the eight branch

---
+++ monitor-server/src/main.py
@@ -0,0 +1,35 @@
+from http.client import HTTPException
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+emails = [{
+        "topic": "jobs emails",
+        "address" : "email address",
+        "detail" : "new jobs with positions"
+    },{
+        "topic": "salary emails",
+        "address" : "email address",
+        "detail" : "salary deatils"
+    },{
+        "topic": "workers benefit emails",
+        "address" : "email address",
+        "detail" : "new benefits list"
+    }]
+
+
+@app.get("/email")
+def emaillist():
+    return emails
+
+@app.get("/emails/{topic}")
+def emaillist(topic):
+    for email in emails :
+        if email["topic"] == topic:
+            return email      
+    raise HTTPException(status_code=404,detail="email with topic" + topic + "not found")         
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
