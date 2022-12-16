fea216829a4701100c9458b316517dcd099177c5
Jay2cof <justice101onuoha@gmail.com>
2022-12-16 15:08:56 +0100

    updated s3 buckets

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,11 +1,13 @@
 resource "aws_lambda_function" "email-monitor" {
   function_name = "email-monitor"
   filename =  "build/job_crawler.zip" 
-  handler = "lambda.lambda_handler"
+  handler = "crawler.handler"
   timeout = 300
   role = local.iam_role
   runtime = "python3.9"
   source_code_hash = filebase64sha256("build/job_crawler.zip")
+
+  layers = [aws_lambda_layer_version.requests_layer.arn]
 
   environment {
     variables = {
@@ -15,7 +17,7 @@ resource "aws_lambda_function" "email-monitor" {
   }
 }
 
-resource "aws_lambda_layer_version" "request_layer" {
+resource "aws_lambda_layer_version" "requests_layer" {
   s3_bucket = "email-monitor-jay"
   s3_key    = "requests-layer.zip"
   layer_name = "requests-layer"
